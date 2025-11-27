40d49f78f760904d8aa50d0a515b6caf30ba78ef
Br_Kristof_02 <brandhuber.kristof_2022b@bolyai-szakkozep.hu>
2025-11-27 20:16:35 +0100

    base

---
+++ tablazatokjs/row.js
@@ -0,0 +1,22 @@
+const array =[
+    {
+        city: "Athén",
+        branch1: "politika",
+        branch2: "tudomány",
+        branch1ex: "demokrácia",
+        branch2ex: "filozófia"
+
+    },
+    {
+        city: "Egyiptom  ",
+        branch1: "mezőgazdaság",
+        branch1ex: "csatornák",
+    },
+    {
+        city: "Spárta",
+        branch1: "neveléstudomány",
+        branch2: "harcászat",
+        branch1ex: "agogé",
+        branch2ex: "hoplita",
+    }
+]
